50715bf88302bfd9e502eba66e83db75b435955a
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-14 16:21:01 +0900

    Update get_files_with_extension_recursive.py

--- get_files_with_extension_recursive.py
+++ get_files_with_extension_recursive.py
@@ -20,23 +20,22 @@ def get_files_with_extension_recursive(archive, extension):
         for member in archive_obj.infolist():
             if member.filename.endswith(extension):
                 file_paths.append(member.filename)
-            elif zipfile.is_zipfile(io.BytesIO(archive_obj.read(member))) or tarfile.is_tarfile(io.BytesIO(archive_obj.read(member))):
+            elif member.filename.endswith(('.zip', '.tar.gz', '.tar.bz2')):
                 with archive_obj.open(member) as nested_archive:
-                    file_paths.extend(get_files_with_extension_recursive(nested_archive, extension))
+                    nested_archive_data = io.BytesIO(nested_archive.read())
+                    file_paths.extend(get_files_with_extension_recursive(nested_archive_data, extension))
     elif isinstance(archive_obj, tarfile.TarFile):
         for member in archive_obj.getmembers():
             if member.isfile() and member.name.endswith(extension):
                 file_paths.append(member.name)
-            elif member.isfile():
-                try:
-                    with archive_obj.extractfile(member) as nested_archive:
-                        file_paths.extend(get_files_with_extension_recursive(nested_archive, extension))
-                except Exception as e:
-                    pass
+            elif member.isfile() and member.name.endswith(('.zip', '.tar.gz', '.tar.bz2')):
+                with archive_obj.extractfile(member) as nested_archive:
+                    nested_archive_data = io.BytesIO(nested_archive.read())
+                    file_paths.extend(get_files_with_extension_recursive(nested_archive_data, extension))
     return file_paths
 
 def main():
-    archive = 'path/to/compressed/file'  # 圧縮ファイルのパスを指定
+    archive = 'path/to/compressed/file.zip'  # 圧縮ファイルのパスを指定
     extension = '.txt'  # 目的の拡張子を指定
 
     file_paths = get_files_with_extension_recursive(archive, extension)
