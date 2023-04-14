6aa3298d4bd8531daf14053afcb18d290e48fa73
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-14 16:46:32 +0900

    Update get_files_with_extension_recursive.py

--- get_files_with_extension_recursive.py
+++ get_files_with_extension_recursive.py
@@ -2,26 +2,27 @@ import io
 import zipfile
 import tarfile
 
-def get_files_with_extension_recursive(archive, extension):
-    file_paths = []
-
+def open_archive(archive):
     if isinstance(archive, io.BytesIO):
         archive.seek(0)
         if zipfile.is_zipfile(archive):
-            archive_obj = zipfile.ZipFile(archive, 'r')
+            return zipfile.ZipFile(archive, 'r')
         elif tarfile.is_tarfile(archive):
-            archive_obj = tarfile.open(fileobj=archive, mode='r:*')
+            return tarfile.open(fileobj=archive, mode='r:*')
         else:
-            return file_paths
+            return None
     elif isinstance(archive, str):
         if zipfile.is_zipfile(archive):
-            archive_obj = zipfile.ZipFile(archive, 'r')
+            return zipfile.ZipFile(archive, 'r')
         elif tarfile.is_tarfile(archive):
-            archive_obj = tarfile.open(archive, 'r:*')
+            return tarfile.open(archive, 'r:*')
         else:
             raise ValueError("Unsupported file format")
     else:
         raise ValueError("Unsupported archive type")
+
+def search_files_in_archive(archive_obj, extension):
+    file_paths = []
 
     if isinstance(archive_obj, zipfile.ZipFile):
         for member in archive_obj.infolist():
@@ -30,7 +31,9 @@ def get_files_with_extension_recursive(archive, extension):
             elif member.filename.endswith(('.zip', '.tar.gz', '.tar.bz2')):
                 with archive_obj.open(member) as nested_archive:
                     nested_archive_data = io.BytesIO(nested_archive.read())
-                    file_paths.extend(get_files_with_extension_recursive(nested_archive_data, extension))
+                    nested_archive_obj = open_archive(nested_archive_data)
+                    if nested_archive_obj:
+                        file_paths.extend(search_files_in_archive(nested_archive_obj, extension))
     elif isinstance(archive_obj, tarfile.TarFile):
         for member in archive_obj.getmembers():
             if member.isfile() and member.name.endswith(extension):
@@ -38,17 +41,23 @@ def get_files_with_extension_recursive(archive, extension):
             elif member.isfile() and member.name.endswith(('.zip', '.tar.gz', '.tar.bz2')):
                 with archive_obj.extractfile(member) as nested_archive:
                     nested_archive_data = io.BytesIO(nested_archive.read())
-                    file_paths.extend(get_files_with_extension_recursive(nested_archive_data, extension))
+                    nested_archive_obj = open_archive(nested_archive_data)
+                    if nested_archive_obj:
+                        file_paths.extend(search_files_in_archive(nested_archive_obj, extension))
     
     archive_obj.close()
     return file_paths
 
 def main():
     archive = 'path/to/compressed/file.zip'  # 圧縮ファイルのパスを指定
-    extension = '.txt'  # 目的の拡張子を指定
+    extension = '.log'  # 目的の拡張子を指定
 
-    file_paths = get_files_with_extension_recursive(archive, extension)
-    print(file_paths)
+    archive_obj = open_archive(archive)
+    if archive_obj:
+        file_paths = search_files_in_archive(archive_obj, extension)
+        print(file_paths)
+    else:
+        print("Failed to open the archive.")
 
 if __name__ == '__main__':
     main()
