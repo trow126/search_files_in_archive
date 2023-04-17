63c2bccd720e70fe34564459847f64c060ef6bcb
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-17 15:32:57 +0900

    Update get_files_with_extension_recursive.py

--- get_files_with_extension_recursive.py
+++ get_files_with_extension_recursive.py
@@ -49,23 +49,23 @@ def search_files_in_archive(archive_obj, extension, current_path=''):
                 if name.endswith(extension):
                     file_paths.append(current_path + '/' + name)
                 elif name.endswith(('.zip', '.tar', '.tar.gz', '.tar.bz2', '.gz', '.bz2')):
-                    with archive_obj.extractfile(member) as nested_archive:
-                        nested_archive_data = io.BytesIO(nested_archive.read())
-                        nested_archive_obj = open_archive(nested_archive_data, name)
-                        if nested_archive_obj:
-                            file_paths.extend(search_files_in_archive(nested_archive_obj, extension, current_path + '/' + name))
+                    nested_archive_data = io.BytesIO(archive_obj.read(member))
+                    nested_archive_obj = open_archive(nested_archive_data, name)
+                    if nested_archive_obj:
+                        file_paths.extend(search_files_in_archive(nested_archive_obj, extension, current_path + '/' + name))
             else:
-                with archive_obj.extractfile(member) as nested_folder:
-                    if nested_folder:
-                        folder_data = io.BytesIO(nested_folder.read())
-                        folder_obj = open_archive(folder_data, name)
-                        if folder_obj:
-                            file_paths.extend(search_files_in_archive(folder_obj, extension, current_path + '/' + name))
+                if isinstance(archive_obj, tarfile.TarFile):
+                    with archive_obj.extractfile(member) as nested_folder:
+                        if nested_folder:
+                            folder_data = io.BytesIO(nested_folder.read())
+                            folder_obj = open_archive(folder_data, name)
+                            if folder_obj:
+                                file_paths.extend(search_files_in_archive(folder_obj, extension, current_path + '/' + name))
 
     return file_paths
 
 
-file_data = open('your_file.zip', 'rb')
-archive_obj = open_archive(file_data, 'your_file.zip')
+file_data = open('your_compressed_file', 'rb')  # 圧縮ファイルの名前を指定
+archive_obj = open_archive(file_data, 'your_compressed_file')  # 圧縮ファイルの名前を指定
 file_paths = search_files_in_archive(archive_obj, '.log')
 print(file_paths)
