10dc45de30505a7075205fbef434a0e7ff2e8583
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-17 14:13:47 +0900

    Update open_archive.py

--- open_archive.py
+++ open_archive.py
@@ -1,3 +1,9 @@
+import zipfile
+import tarfile
+import gzip
+import bz2
+import io
+
 def open_archive(file_data):
     try:
         archive_obj = zipfile.ZipFile(file_data)
@@ -12,6 +18,22 @@ def open_archive(file_data):
         print("Opened as TAR")
         return archive_obj
     except tarfile.ReadError:
+        pass
+
+    file_data.seek(0)
+    try:
+        archive_obj = gzip.GzipFile(fileobj=file_data)
+        print("Opened as GZ")
+        return archive_obj
+    except OSError:
+        pass
+
+    file_data.seek(0)
+    try:
+        archive_obj = bz2.BZ2File(file_data)
+        print("Opened as BZ2")
+        return archive_obj
+    except OSError:
         pass
 
     return None
