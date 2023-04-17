49b901aae1919eb9fe61cde7db1918f26e69c43b
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-17 14:32:43 +0900

    Update search_files_in_archive.py

--- search_files_in_archive.py
+++ search_files_in_archive.py
@@ -1,31 +1,15 @@
-def search_files_in_archive(archive_obj, extension, parent=""):
-    file_paths = []
-
-    if isinstance(archive_obj, zipfile.ZipFile):
-        for member in archive_obj.infolist():
-            print("Processing ZIP member:", member.filename)
-            current_path = f"{parent}/{member.filename}"
-            if member.filename.endswith(extension):
-                print(f"Found target file: {current_path}")
-                file_paths.append(current_path)
-             elif member.name.endswith(('.zip', '.tar', '.tar.gz', '.tar.bz2', '.gz', '.bz2', '.tar.gz.bz2')):
-                with archive_obj.extractfile(member) as nested_archive:
-                    nested_archive_data = io.BytesIO(nested_archive.read())
-                    nested_archive_obj = open_archive(nested_archive_data, member.name)
-                    if nested_archive_obj:
-                        file_paths.extend(search_files_in_archive(nested_archive_obj, extension, current_path))
-    elif isinstance(archive_obj, tarfile.TarFile):
-        for member in archive_obj.getmembers():
-            print("Processing TAR member:", member.name)
-            current_path = f"{parent}/{member.name}"
-            if member.name.endswith(extension):
-                print(f"Found target file: {current_path}")
-                file_paths.append(current_path)
-             elif member.name.endswith(('.zip', '.tar', '.tar.gz', '.tar.bz2', '.gz', '.bz2', '.tar.gz.bz2')):
-                with archive_obj.extractfile(member) as nested_archive:
-                    nested_archive_data = io.BytesIO(nested_archive.read())
-                    nested_archive_obj = open_archive(nested_archive_data, member.name)
-                    if nested_archive_obj:
-                        file_paths.extend(search_files_in_archive(nested_archive_obj, extension, current_path))
-
-    return file_paths
+def search_files_in_archive(archive_obj, extension, current_path=''):
+    ...
+    for member in archive_obj.getmembers():
+        ...
+        elif member.name.endswith(('.zip', '.tar', '.tar.gz', '.tar.bz2', '.gz', '.bz2', '.tar.gz.bz2')):
+            with archive_obj.extractfile(member) as nested_archive:
+                nested_archive_data = io.BytesIO(nested_archive.read())
+                if hasattr(nested_archive, 'name'):
+                    nested_archive_name = nested_archive.name
+                else:
+                    nested_archive_name = member.name
+                nested_archive_obj = open_archive(nested_archive_data, nested_archive_name)
+                if nested_archive_obj:
+                    file_paths.extend(search_files_in_archive(nested_archive_obj, extension, current_path))
+        ...
