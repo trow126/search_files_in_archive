cc375c2c0c44d1d74089b3a990e5b06f69788c92
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 18:38:37 +0900

    Update tmp.py

--- search_files_in_archive/tmp.py
+++ search_files_in_archive/tmp.py
@@ -18,6 +18,32 @@ def search_files_in_archive(path_or_data, target_ext, parent_path='', file_name=
             return []
 
     if archive_obj:
-        # (search_files_in_archive の処理は変更なし)
+        if isinstance(archive_obj, zipfile.ZipFile):
+            for member in archive_obj.infolist():
+                full_path = parent_path + '/' + member.filename
+                if member.filename.endswith(target_ext):
+                    file_paths.append(full_path)
+                elif not member.is_dir():
+                    with archive_obj.open(member) as file_data:
+                        inner_archive = open_archive(file_data, member.filename)
+                        if inner_archive:
+                            file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path))
+        elif isinstance(archive_obj, tarfile.TarFile):
+            for member in archive_obj.getmembers():
+                full_path = parent_path + '/' + member.name
+                if member.name.endswith(target_ext):
+                    file_paths.append(full_path)
+                    file_data = archive_obj.extractfile(member)
+                elif not member.isdir():
+                    file_data = archive_obj.extractfile(member)
+                    if file_data:
+                        inner_archive = open_archive(file_data, member.name)
+                        if inner_archive:
+                            file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path))
+        elif isinstance(archive_obj, (gzip.GzipFile, bz2.BZ2File)):
+            file_data = io.BytesIO(archive_obj.read())
+            inner_archive = open_archive(file_data)
+            if inner_archive:
+                file_paths.extend(search_files_in_archive(inner_archive, target_ext, parent_path))
 
     return file_paths
