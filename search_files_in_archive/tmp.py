6c8f8c5a316734f7c1fa5f0d758e83243771cfa7
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 19:05:21 +0900

    Update tmp.py

--- search_files_in_archive/tmp.py
+++ search_files_in_archive/tmp.py
@@ -4,7 +4,8 @@ if isinstance(archive_obj, zipfile.ZipFile):
         if member.filename.endswith(target_ext):
             file_paths.append(full_path)
         elif not member.is_dir():
-            file_data = io.BytesIO(archive_obj.read(member))
-            inner_archive = open_archive(file_data, member.filename)
-            if inner_archive:
-                file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, file_name=member.filename))
+            with archive_obj.open(member) as file_data:
+                file_data = io.BytesIO(file_data.read())
+                inner_archive = open_archive(file_data, member.filename)
+                if inner_archive:
+                    file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, file_name=member.filename))
