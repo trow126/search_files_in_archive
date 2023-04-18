c00ad08c2f2f7627e0f80d07ccaab197f0df4f79
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 11:53:25 +0900

    Update to_dataframe.py

--- to_dataframe.py
+++ to_dataframe.py
@@ -1,11 +1,11 @@
-def search_files_in_archive(archive_obj, target_ext, parent_path=''):
-    file_paths = []
-    dataframes = defaultdict(pd.DataFrame)  # カラム数ごとにDataFrameをまとめる辞書
+import io
+import pandas as pd
 
-    def process_dataframe(data):
-        dataframe = pd.read_csv(io.StringIO(data), header=None)
-        num_columns = len(dataframe.columns)
-        dataframes[num_columns] = dataframes[num_columns].append(dataframe, ignore_index=True)
+def search_files_in_archive(archive_obj, target_ext, parent_path='', df_dict=None):
+    if df_dict is None:
+        df_dict = {}
+
+    file_paths = []
 
     if isinstance(archive_obj, zipfile.ZipFile):
         for member in archive_obj.infolist():
@@ -14,15 +14,16 @@ def search_files_in_archive(archive_obj, target_ext, parent_path=''):
                 file_paths.append(full_path)
                 with archive_obj.open(member) as file_data:
                     data = file_data.read().decode()
-                    process_dataframe(data)
+                    dataframe = pd.read_csv(io.StringIO(data), header=None)
+                    num_columns = len(dataframe.columns)
+                    if num_columns not in df_dict:
+                        df_dict[num_columns] = []
+                    df_dict[num_columns].append(dataframe)
             elif not member.is_dir():
                 with archive_obj.open(member) as file_data:
                     inner_archive = open_archive(file_data, member.filename)
                     if inner_archive:
-                        inner_file_paths, inner_dataframes = search_files_in_archive(inner_archive, target_ext, full_path)
-                        file_paths.extend(inner_file_paths)
-                        for num_columns, inner_dataframe in inner_dataframes.items():
-                            dataframes[num_columns] = dataframes[num_columns].append(inner_dataframe, ignore_index=True)
+                        file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, df_dict))
     elif isinstance(archive_obj, tarfile.TarFile):
         for member in archive_obj.getmembers():
             full_path = parent_path + '/' + member.name
@@ -31,27 +32,36 @@ def search_files_in_archive(archive_obj, target_ext, parent_path=''):
                 file_data = archive_obj.extractfile(member)
                 if file_data:
                     data = file_data.read().decode()
-                    process_dataframe(data)
+                    dataframe = pd.read_csv(io.StringIO(data), header=None)
+                    num_columns = len(dataframe.columns)
+                    if num_columns not in df_dict:
+                        df_dict[num_columns] = []
+                    df_dict[num_columns].append(dataframe)
             elif not member.isdir():
                 file_data = archive_obj.extractfile(member)
                 if file_data:
                     inner_archive = open_archive(file_data, member.name)
                     if inner_archive:
-                        inner_file_paths, inner_dataframes = search_files_in_archive(inner_archive, target_ext, full_path)
-                        file_paths.extend(inner_file_paths)
-                        for num_columns, inner_dataframe in inner_dataframes.items():
-                            dataframes[num_columns] = dataframes[num_columns].append(inner_dataframe, ignore_index=True)
+                        file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, df_dict))
     elif isinstance(archive_obj, (gzip.GzipFile, bz2.BZ2File)):
         file_data = io.BytesIO(archive_obj.read())
         inner_archive = open_archive(file_data)
         if inner_archive:
-            inner_file_paths, inner_dataframes = search_files_in_archive(inner_archive, target_ext, parent_path)
-            file_paths.extend(inner_file_paths)
-            for num_columns, inner_dataframe in inner_dataframes.items():
-                dataframes[num_columns] = dataframes[num_columns].append(inner_dataframe, ignore_index=True)
+            file_paths.extend(search_files_in_archive(inner_archive, target_ext, parent_path, df_dict))
 
-    return file_paths, dataframes
+    return file_paths,df_dict
 
 
 
+
+def main():
+    ...
+    if archive_obj:
+        df_dict = search_files_in_archive(archive_obj, extension)
+        
+        for num_columns, dataframes in df_dict.items():
+            combined_df = pd.concat(dataframes, ignore_index=True)
+            print(f"Number of columns: {num_columns}")
+            print(combined_df.head())  # 先頭部分のみを表示。全て表示する場合は、`print(combined_df)`に変更。
+    ...
 
