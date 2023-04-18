db7f46d7ebc2b9ba317d4784fa313d873a007289
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 18:34:56 +0900

    Update tmp.py

--- search_files_in_archive/tmp.py
+++ search_files_in_archive/tmp.py
@@ -1,50 +1,23 @@
-import io
-import os
-import pandas as pd
-import zipfile
-import tarfile
-import gzip
-import bz2
-
-
-def open_archive(file_data, file_name=None):
-    # (中略) この部分は変更されていません
-
-
-def search_files_in_archive(path, target_ext, parent_path=''):
+def search_files_in_archive(path_or_data, target_ext, parent_path='', file_name=None):
     file_paths = []
 
-    if os.path.isdir(path):
-        for item in os.listdir(path):
-            item_path = os.path.join(path, item)
-            if os.path.isdir(item_path):
-                file_paths.extend(search_files_in_archive(item_path, target_ext, item_path))
-            else:
-                with open(item_path, 'rb') as file_data:
-                    archive_obj = open_archive(file_data, item)
-                    if archive_obj:
-                        file_paths.extend(search_files_in_archive(archive_obj, target_ext, item_path))
-                    elif item_path.endswith(target_ext):
-                        file_paths.append(item_path)
+    if isinstance(path_or_data, io.BytesIO):
+        file_data = path_or_data
+        archive_obj = open_archive(file_data, file_name)
     else:
-        with open(path, 'rb') as file_data:
-            archive_obj = open_archive(file_data, path)
-            if archive_obj:
-                file_paths.extend(search_files_in_archive(archive_obj, target_ext, parent_path))
-            elif path.endswith(target_ext):
-                file_paths.append(path)
+        if os.path.isfile(path_or_data):
+            with open(path_or_data, 'rb') as file_data:
+                archive_obj = open_archive(file_data, path_or_data)
+        elif os.path.isdir(path_or_data):
+            for root, _, files in os.walk(path_or_data):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    file_paths.extend(search_files_in_archive(file_path, target_ext))
+            return file_paths
+        else:
+            return []
+
+    if archive_obj:
+        # (search_files_in_archive の処理は変更なし)
 
     return file_paths
-
-
-def main():
-    path = 'path/to/compressed/file.zip'  # 圧縮ファイルまたはフォルダのパスを指定
-    extension = '.log'  # 目的の拡張子を指定
-
-    file_paths = search_files_in_archive(path, extension)
-    for path in file_paths:
-        print("Found file:", path)
-
-
-if __name__ == '__main__':
-    main()
