31bb78730173fb1ff9b7d8257b3a97184e42e1fa
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 16:35:12 +0900

    Update search_files_in_archive_new.py

--- search_files_in_archive/search_files_in_archive_new.py
+++ search_files_in_archive/search_files_in_archive_new.py
@@ -8,6 +8,31 @@ import bz2
 import datetime
 from collections import deque
 
+def open_archive(path):
+    try:
+        return zipfile.ZipFile(path)
+    except zipfile.BadZipFile:
+        pass
+
+    try:
+        return tarfile.open(path)
+    except tarfile.ReadError:
+        pass
+
+    try:
+        if path.endswith('.gz'):
+            return gzip.open(path)
+    except OSError:
+        pass
+
+    try:
+        if path.endswith('.bz2'):
+            return bz2.open(path)
+    except OSError:
+        pass
+
+    return None
+
 def search_files_in_archive(path, target_ext, parent_path=''):
     file_paths = []
     paths_to_process = deque([(path, parent_path)])
@@ -19,23 +44,25 @@ def search_files_in_archive(path, target_ext, parent_path=''):
             if current_path.endswith(target_ext):
                 file_paths.append(current_parent_path + '/' + os.path.basename(current_path))
 
-            try:
-                archive_obj = zipfile.ZipFile(current_path)
+            archive_obj = open_archive(current_path)
+
+            if isinstance(archive_obj, zipfile.ZipFile):
                 for member in archive_obj.namelist():
                     full_path = current_parent_path + '/' + member
                     if member.endswith(target_ext):
                         file_paths.append(full_path)
-            except zipfile.BadZipFile:
-                pass
 
-            try:
-                archive_obj = tarfile.open(current_path)
+            elif isinstance(archive_obj, tarfile.TarFile):
                 for member in archive_obj.getnames():
                     full_path = current_parent_path + '/' + member
                     if member.endswith(target_ext):
                         file_paths.append(full_path)
-            except tarfile.ReadError:
-                pass
+
+            elif isinstance(archive_obj, (gzip.GzipFile, bz2.BZ2File)):
+                file_data = io.BytesIO(archive_obj.read())
+                inner_archive = open_archive(file_data)
+                if inner_archive:
+                    file_paths.extend(search_files_in_archive(inner_archive, target_ext, parent_path))
 
         elif os.path.isdir(current_path):
             with os.scandir(current_path) as entries:
