d850d45f25bd4dc24d04ebd5124dfc01588d4252
trow126 <43240931+trow126@users.noreply.github.com>
2023-04-18 18:23:43 +0900

    Update search_files_in_archive_new.py

--- search_files_in_archive/search_files_in_archive_new.py
+++ search_files_in_archive/search_files_in_archive_new.py
@@ -5,72 +5,58 @@ import zipfile
 import tarfile
 import gzip
 import bz2
-import datetime
 
-def open_archive(file_data, file_name=None):
-    # 省略: open_archiveのコードは変更せず、そのまま使います
+# (open_archive 関数は変更なし)
 
-def search_files_in_archive(path, target_ext, parent_path='', compressed_path=None):
-    if isinstance(path, str):  # ファイルパスが渡された場合
-        if os.path.isfile(path):  # ファイルが存在する場合
-            with open(path, 'rb') as f:
-                archive_obj = open_archive(f)
-                if archive_obj:
-                    return search_files_in_archive(archive_obj, target_ext, parent_path, compressed_path=path)
-                else:
-                    return []
-        elif os.path.isdir(path):  # ディレクトリが存在する場合
-            file_paths = []
-            for root, _, files in os.walk(path):
-                for file in files:
-                    if file.endswith(target_ext):
-                        file_paths.append(os.path.join(root, file))
-            return file_paths
-        else:
-            return []
-
+def search_files_in_archive(path, target_ext, parent_path=''):
     file_paths = []
 
-    if isinstance(path, zipfile.ZipFile):
-        for member in path.infolist():
-            full_path = (compressed_path + ':' if compressed_path else '') + parent_path + '/' + member.filename
-            if member.filename.endswith(target_ext):
-                file_paths.append(full_path)
-            elif not member.is_dir():
-                with path.open(member) as file_data:
-                    inner_archive = open_archive(file_data, member.filename)
-                    if inner_archive:
-                        file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, compressed_path))
-    elif isinstance(path, tarfile.TarFile):
-        for member in path.getmembers():
-            full_path = (compressed_path + ':' if compressed_path else '') + parent_path + '/' + member.name
-            if member.name.endswith(target_ext):
-                file_paths.append(full_path)
-            elif not member.isdir():
-                file_data = path.extractfile(member)
-                if file_data:
-                    inner_archive = open_archive(file_data, member.name)
-                    if inner_archive:
-                        file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path, compressed_path))
-    elif isinstance(path, (gzip.GzipFile, bz2.BZ2File)):
-        file_data = io.BytesIO(path.read())
-        inner_archive = open_archive(file_data)
-        if inner_archive:
-            file_paths.extend(search_files_in_archive(inner_archive, target_ext, parent_path, compressed_path))
+    if os.path.isfile(path):
+        archive_obj = open_archive(path)
+        if archive_obj:
+            if isinstance(archive_obj, zipfile.ZipFile):
+                for member in archive_obj.infolist():
+                    full_path = parent_path + '/' + member.filename
+                    if member.filename.endswith(target_ext):
+                        file_paths.append(full_path)
+                    elif not member.is_dir():
+                        with archive_obj.open(member) as file_data:
+                            inner_archive = open_archive(file_data, member.filename)
+                            if inner_archive:
+                                file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path))
+            elif isinstance(archive_obj, tarfile.TarFile):
+                for member in archive_obj.getmembers():
+                    full_path = parent_path + '/' + member.name
+                    if member.name.endswith(target_ext):
+                        file_paths.append(full_path)
+                        file_data = archive_obj.extractfile(member)
+                    elif not member.isdir():
+                        file_data = archive_obj.extractfile(member)
+                        if file_data:
+                            inner_archive = open_archive(file_data, member.name)
+                            if inner_archive:
+                                file_paths.extend(search_files_in_archive(inner_archive, target_ext, full_path))
+            elif isinstance(archive_obj, (gzip.GzipFile, bz2.BZ2File)):
+                file_data = io.BytesIO(archive_obj.read())
+                inner_archive = open_archive(file_data)
+                if inner_archive:
+                    file_paths.extend(search_files_in_archive(inner_archive, target_ext, parent_path))
+
+    elif os.path.isdir(path):
+        for root, _, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_paths.extend(search_files_in_archive(file_path, target_ext))
 
     return file_paths
 
 def main():
-    path = 'path/to/compressed/or/directory'  # 圧縮ファイルまたはディレクトリのパスを指定
+    path = 'path/to/compressed/file_or_folder'  # 圧縮ファイルまたはフォルダのパスを指定
     extension = '.log'  # 目的の拡張子を指定
 
     file_paths = search_files_in_archive(path, extension)
-    for path in file_paths:
-        print("Found file:", path)
-
-    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    with open(f'{now}_file_paths.txt', 'w') as f:
-        f.write('\n'.join(file_paths))
+    for file_path in file_paths:
+        print("Found file:", file_path)
 
 if __name__ == '__main__':
     main()
